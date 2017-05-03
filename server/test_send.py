6e7fdcd6845fdc0283af2fffbb910051e7368d44
Roman Mindlin <mindlin@inbox.ru>
2017-05-03 18:36:51 +0300

    test_send

--- server/test_send.py
+++ server/test_send.py
@@ -6,13 +6,17 @@ import requests
 import datetime
 
 
+SERVER_URL = 'http://10.0.100.102:5000/api/add_message/'
+
 json = {
     'raspi_serial': 'ERROR000000000',
-    'ibeacon_serial': 'b3de1433880f020106030220fe07fffe',
+    'ibeacon_uuid': 'b3de1433880f020106030220fe07fffe',
+    'ibeacon_major': 1,
+    'ibeacon_minor': 5555,
     'in_time': (datetime.datetime.now() - datetime.timedelta(minutes=1)).isoformat(),
     'out_time': datetime.datetime.now().isoformat()
 }
 
-res = requests.post('http://localhost:5000/api/add_message/', json=json)
+res = requests.post(SERVER_URL, json=json)
 if res.ok:
     print(res)
